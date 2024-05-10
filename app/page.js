c601292eefe9d6d188d8d50daa6f1dc895cea0f0
Manan Gandhi <ardumanan@gmail.com>
2024-05-10 11:04:02 +0530

    Fixed typo

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-center p-5 md:p-24 bg-background">
 			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight leading-relaxed lg:text-5xl">
-				Welcome to Our Submition for the{" "}
+				Welcome to Our Submission for the{" "}
 				<span className=" bg-primary text-primary-foreground p-2 rounded">
 					EdgeDB Hackathon!
 				</span>
